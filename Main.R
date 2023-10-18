bf33ec3354e6b586e189a97681934c58e9415b48
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-10-18 16:06:26 +0200

    upates

--- Main.R
+++ Main.R
@@ -9,13 +9,14 @@ orangePath_value <- sub("^orangePortfolioPath=\\s*", "", paths_content[orangePat
 DT <- read_doc_and_save_df(orangePath_value)
 
 #Saving the relevant sheets as dataframes
-original.BorrowerData <- DT$`Borrower Data`
-original.LoansData <- DT$`Loans Data`
-original.CoOwnersData <- DT$`Co-owners Data`
-original.GuarantorsData <- DT$`Guarantors data`
-original.GuaranteesData <- DT$`Guarantees Data`
+original.BorrowerData <- DT$`Borrower Data` %>% mutate_all(tolower)
+original.LoansData <- DT$`Loans Data` %>% mutate_all(tolower)
+original.CoOwnersData <- DT$`Co-owners Data` %>% mutate_all(tolower)
+original.GuarantorsData <- DT$`Guarantors data`%>% mutate_all(tolower)
+original.GuaranteesData <- DT$`Guarantees Data`%>% mutate_all(tolower)
 
 source("tables/Loans.R")
 source("tables/Counterparties.R")
 source("tables/Entities.R")
 source("link.counterparties.entities.R")
+
--- tables/link.loans.counterparties.R
+++ tables/link.loans.counterparties.R
@@ -1,1 +1,4 @@
-link.loans.counterparties
+link.loans.counterparties <- COUNTERPARTIES %>% ungroup() %>% select (id.counterparty, id.bor) 
+link.loans.counterparties <- link.loans.counterparties %>% 
+  inner_join(LOANS %>% select(id.loan, id.bor), by= "id.bor")
+link.loans.counterparties <- link.loans.counterparties %>% select(-id.bor)
