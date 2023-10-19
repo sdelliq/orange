794957f32bdbfb3910dfca106efab15097a1feb2
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-10-19 16:34:51 +0200

    we're not even using this anymore

--- tables/link.counterparties.entities.R
+++ tables/link.counterparties.entities.R
@@ -1,5 +1,6 @@
-link.counterparties.entities <- COUNTERPARTIES %>% ungroup() %>% select (id.counterparty, cf.piva) 
+link.counterparties.entities <- COUNTERPARTIES %>% select (id.counterparty, cf.piva) 
 link.counterparties.entities <- divide_column_by_character(link.counterparties.entities, cf.piva, ",")
 link.counterparties.entities <- link.counterparties.entities %>% 
   left_join(ENTITIES %>% select(cf.piva, id.entity), by= "cf.piva")
 link.counterparties.entities <- link.counterparties.entities %>% select(-cf.piva)
+COUNTERPARTIES <- COUNTERPARTIES %>% select(-cf.piva)
